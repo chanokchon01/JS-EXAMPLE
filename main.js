9250171481b5b3258c0ae525c670f2faef99e216
chanokchon01 <pluemmmz@gmail.com>
2022-07-12 13:29:26 +0700

    add img and some index

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
-import './style.css'
+import './style.css';
+import coinicon from './assets/coin.png'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+
+const btnCoin = document.getElementById("btn-coin");
+const btnBill = document.getElementById("btn-bill");
+const WalletElement = document.getElementById("wallet");
+
+/*
+btnCoin.addEventListener("click",function(){
+  let iconElemetHtml = <img src="$(iconCoinUrl)"/>;
+  WalletElement.insertAdjacentElement("beforeend",iconElemetHtml);
+});
+
+btnBill.addEventListener("click",function(){
+  let iconElemetHtml = <img src="$(iconBillUrl)"/>;
+  WalletElement.insertAdjacentElement("beforeend",iconElemetHtml);
+});*/
+
+btnCoin.addEventListener("click",function(){
+  const coinElement = document.createElement('img')
+  coinElement.setAttribute('src',"https://i.ibb.co/zNmpdCq/Pngtree-100-dollar-bill-7312202.png")
+  //document.body.appendChild(coinElement);
+  document.getElementById(wallet).appendChild(coinElement);
+});
+
