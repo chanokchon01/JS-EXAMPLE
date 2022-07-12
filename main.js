6c2c7972f66a85d987877c1c17627d06300bfaa0
chanokchon01 <pluemmmz@gmail.com>
2022-07-12 22:22:51 +0700

    make it work and somr css

--- main.js
+++ main.js
@@ -1,26 +1,45 @@
 import './style.css';
-import coinicon from './assets/coin.png'
+import JSConfetti from 'js-confetti'
+const jsConfetti = new JSConfetti()
+
 
 
 const btnCoin = document.getElementById("btn-coin");
 const btnBill = document.getElementById("btn-bill");
 const WalletElement = document.getElementById("wallet");
 
-/*
-btnCoin.addEventListener("click",function(){
-  let iconElemetHtml = <img src="$(iconCoinUrl)"/>;
-  WalletElement.insertAdjacentElement("beforeend",iconElemetHtml);
-});
+const min = Math.ceil(300);
+const max = Math.floor(500);
+const targetRich = Math.floor(Math.random()*(max-min)+min);
 
-btnBill.addEventListener("click",function(){
-  let iconElemetHtml = <img src="$(iconBillUrl)"/>;
-  WalletElement.insertAdjacentElement("beforeend",iconElemetHtml);
-});*/
+function updateSavings(value){
+  const savingsElement = document.getElementById("savings")
+  const currentValue = parseInt(savingsElement.textContent);
+
+  let newValue = currentValue+value;
+  savingsElement.textContent = newValue;
+
+  if(newValue > targetRich){
+    jsConfetti.addConfetti()
+    alert("คุณรวยแล้ว")
+  }
+}
 
 btnCoin.addEventListener("click",function(){
   const coinElement = document.createElement('img')
-  coinElement.setAttribute('src',"https://i.ibb.co/zNmpdCq/Pngtree-100-dollar-bill-7312202.png")
-  //document.body.appendChild(coinElement);
-  document.getElementById(wallet).appendChild(coinElement);
+  coinElement.setAttribute('src',"https://i.ibb.co/dLDhB15/Pngtree-cartoon-gold-coin-png-download-4463835.png")
+  coinElement.setAttribute('width','70px')
+  console.log(10)
+  
+  document.getElementById('wallet').appendChild(coinElement);
+  updateSavings(10)
+  
+});
+btnBill.addEventListener("click",function(){
+  const billElement = document.createElement('img')
+  billElement.setAttribute('src',"https://i.ibb.co/zNmpdCq/Pngtree-100-dollar-bill-7312202.png")
+  billElement.setAttribute('width','100px')
+  document.getElementById('wallet').appendChild(billElement);
+  updateSavings(100)
 });
 
