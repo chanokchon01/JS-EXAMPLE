25bf262f5ef39ea1e8217ba25a2040660b75b257
chanokchon01 <pluemmmz@gmail.com>
2022-07-13 18:51:55 +0700

    remove empty line and unused code

--- main.js
+++ main.js
@@ -1,40 +1,26 @@
 import './style.css';
 import JSConfetti from 'js-confetti'
 const jsConfetti = new JSConfetti()
-
-
-
 const btnCoin = document.getElementById("btn-coin");
 const btnBill = document.getElementById("btn-bill");
-const WalletElement = document.getElementById("wallet");
-
 const min = Math.ceil(300);
 const max = Math.floor(500);
 const targetRich = Math.floor(Math.random()*(max-min)+min);
-
 function updateSavings(value){
   const savingsElement = document.getElementById("savings")
   const currentValue = parseInt(savingsElement.textContent);
-
   let newValue = currentValue+value;
   savingsElement.textContent = newValue;
-
   if(newValue > targetRich){
     jsConfetti.addConfetti()
-    
-    
   }
 }
-
 btnCoin.addEventListener("click",function(){
   const coinElement = document.createElement('img')
   coinElement.setAttribute('src',"https://i.ibb.co/dLDhB15/Pngtree-cartoon-gold-coin-png-download-4463835.png")
   coinElement.setAttribute('width','70px')
-  console.log(10)
-  
   document.getElementById('wallet').appendChild(coinElement);
   updateSavings(10)
-  
 });
 btnBill.addEventListener("click",function(){
   const billElement = document.createElement('img')
